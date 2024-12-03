3e69ce40411a61f5fd2b0e4fb82da8be696ccc6f
Brett Odgers-Jewell <brett.odgers-jewell@konjur.sg>
2024-12-03 10:32:11 +0800

    update module

--- module.tf
+++ module.tf
@@ -1,19 +1,23 @@
-# module file for Azure Bastion Host module base on the variables.tf file
-
 resource "azurerm_bastion_host" "bastion" {
     name                = module.bastion_name.naming_convention_output[var.naming_convention_info.name].names.0
     resource_group_name = var.resource_group_name
     location            = var.location
     sku                 = var.sku
-    ip_configuration    = var.ip_configuration
-    scale_units         = var.scale_units
-    tags                = var.tags
-    
     copy_paste_enabled     = var.copy_paste_enabled
     file_copy_enabled      = var.file_copy_enabled
     ip_connect_enabled     = var.ip_connect_enabled
     shareable_link_enabled = var.shareable_link_enabled
     tunneling_enabled      = var.tunneling_enabled
+    scale_units         = var.scale_units
+    tags                = var.tags
+    
+    ip_configuration {
+        name = var.ip_configuration.name
+        subnet_id = var.ip_configuration.subnet_id
+        public_ip_address_id = var.ip_configuration.public_ip_address_id
+    }
+
+
 
 
 }
